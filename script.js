aa915fc7a96f5b48b51c20b09a10c4eb9be44767
Jitender <jkggn1996@gmail.com>
2020-02-20 03:58:47 +0530

    Make the first character uppercase of error message

--- script.js
+++ script.js
@@ -28,12 +28,18 @@ function isValidEmail(email){
 function checkRequired(inputArr){
 	inputArr.forEach(function(input){
 		if (input.value.trim() == '') {
-			showError(input, `${input.id} is required`);
+			showError(input, `${getFieldName(input)} is required`);
 		}else{
 			showSuccess(input);
 		}
 
 	});
+}
+
+//Get fieldname
+
+function getFieldName(input){
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 //Event Listeners
